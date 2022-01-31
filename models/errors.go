644ddf78ab9b9cfdf5bf703922ad7d14121f696b
erizkiatama <erizkiatama@gmail.com>
2022-01-31 17:39:38 +0800

    Added response and custom error

---
+++ models/errors.go
@@ -0,0 +1,69 @@
+package models
+
+import (
+	"fmt"
+	"log"
+)
+
+type CustomError interface {
+	LogError()
+	GetStatus() int
+}
+
+type ClientError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Type    string `json:"type"`
+	Status  int    `json:"status"`
+}
+
+func (ths *ClientError) Error() string {
+	return fmt.Sprintf("%s: %s", ths.Code, ths.Message)
+}
+
+func (ths *ClientError) GetStatus() int {
+	return ths.Status
+}
+
+func (ths *ClientError) LogError() {
+	// do nothing
+}
+
+func NewClientError(code, message string, statusCode int) error {
+	return &ClientError{
+		Code:    code,
+		Message: message,
+		Type:    "client",
+		Status:  statusCode,
+	}
+}
+
+type ServerError struct {
+	Code        string `json:"code"`
+	Message     string `json:"message"`
+	Type        string `json:"type"`
+	Status      int    `json:"status"`
+	originalErr error
+}
+
+func (ths *ServerError) Error() string {
+	return fmt.Sprintf("%s: %s", ths.Code, ths.originalErr.Error())
+}
+
+func (ths *ServerError) LogError() {
+	log.Println("ERROR: " + ths.Error())
+}
+
+func (ths *ServerError) GetStatus() int {
+	return ths.Status
+}
+
+func NewServerError(code string, statusCode int, origin error) error {
+	return &ServerError{
+		Code:        code,
+		Message:     "Internal Server Error",
+		Type:        "server",
+		Status:      statusCode,
+		originalErr: origin,
+	}
+}
---
+++ models/response.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Result interface{} `json:"result,omitempty"`
+	Error  error       `json:"error,omitempty"`
+}
+
+func ConstructResponse(c *gin.Context, result interface{}, err error) {
+	var response Response
+	var customErr CustomError
+
+	if err != nil {
+		response.Error = err
+		if ok := errors.As(err, &customErr); !ok {
+			return
+		}
+		customErr.LogError()
+		c.JSON(customErr.GetStatus(), response)
+		return
+	}
+
+	response.Result = result
+	c.JSON(http.StatusOK, response)
+}
